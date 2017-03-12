2f2c54c94812e53ce4c1a82db8e3ae608ae6e5fc
santiago <ojedabotansantiago@gmail.com>
2017-03-12 20:55:51 +0100

    1º
    1º

---
+++ index.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.listen(3000, function(err) {
+    if (err) {
+        console.error('Error listening in port 3000', err);
+        process.exit(1);
+    }
+    console.log('Koa server listening in port 3000');
+});
+app.use(async(ctx, next) => {
+    console.log(`The request url is ${ctx.url}`);
+    ctx.body = 'hola javier ';
+});
