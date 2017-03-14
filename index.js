0b8937e5a1ea32a29de85854e9cf12a407eff1d0
santiago <ojedabotansantiago@gmail.com>
2017-03-14 16:18:27 +0100

    logger & bunyan

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const Koa = require('koa');
+const koaLogger = require('koa-logger');
+const logger = require('./logger.js');
 const app = new Koa();
 
-app.listen(3000, function(err) {
-    if (err) {
-        console.error('Error listening in port 3000', err);
-        process.exit(1);
-    }
-    console.log('Koa server listening in port 3000');
+if (process.env.NODE_ENV === 'dev') {
+    app.use(koaLogger());
+}
+app.use(async(ctx, next) => {
+    const start = Date.now();
+    await next();
+    const time = Date.now() - start;
+    //set the header
+    ctx.set('X-Response-Time', `${time} ms`);
 });
 app.use(async(ctx, next) => {
-    console.log(`The request url is ${ctx.url}`);
-    ctx.body = 'hola javier ';
+    logger.debug(`The request url is ${ctx.url}`);
+    ctx.body = {
+        ok: 1
+    };
+});
+app.listen(3000, function(err) {
+    if (err) {
+        logger.error('Error listening in port 3000', err);
+        process.exit(1);
+    }
+    logger.info('Koa server listening in port 3000');
 });
