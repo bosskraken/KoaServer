834f3a41b557d87283154dba56f9a1c4d970f2d1
santiago <ojedabotansantiago@gmail.com>
2017-03-15 20:32:35 +0100

    sesion

--- index.js
+++ index.js
@@ -20,16 +20,19 @@ app.use(userRouter.routes());
 app.use(mount('/api/v1', userRouter.routes()));
 validate(app);
 /* sesion */
+if (process.env.NODE_ENV === 'dev') {
+    app.use(koaLogger());
+}
 /* definos la clave de la sesion */
 app.keys = ['SAN'];
 /* registramos el middleware de session */
 app.use(convert(session({
     store: new File({
-        sessionDirectory: '/sessions'
+        sessionDirectory: __dirname + '/sessions'
     })
 })));
+
 app.use(async(ctx, next) => {
-    logger.info('entra');
     logger.info(`Last request was ${ctx.session.lastRequest}`);
     ctx.session.lastRequest = new Date();
     await next();
