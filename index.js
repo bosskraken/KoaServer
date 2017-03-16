9ac287a984bc194ed42f6d5646157f8a76b2b817
santiago <ojedabotansantiago@gmail.com>
2017-03-16 03:35:12 +0100

    control error

--- index.js
+++ index.js
@@ -25,8 +25,11 @@ const onDBReady = (err) => {
     if (err) {
         logger.error('Error connecting', err);
         throw new Error('Error connecting', err);
+    } else {
+        console.log("We are connected");
     }
     const app = new Koa();
+
     if (process.env.NODE_ENV === 'dev') {
         app.use(koaLogger());
     }
